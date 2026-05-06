a7f1fa92a2165b63aa6525b705f859243572a36b
Timalm90 <timalm90@gmail.com>
2026-05-06 08:46:12 +0200

    simplified shadows, updated panda.glb

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { Canvas } from '@react-three/fiber'
 import { Model } from './components/scene/PandaModel'
-import { Environment, ContactShadows, useGLTF,OrbitControls } from '@react-three/drei'
+import { Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 useGLTF.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/')
 
@@ -13,20 +13,17 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     
-    <Canvas shadows camera={{ position: [0, -2, 10], fov: 50 }}
+    <Canvas camera={{ position: [0, 0, 8], fov: 50 }}
       style={{ width: '100%', height: '100vh' }}
       gl={{ antialias: true, alpha: false }}
       dpr={[1, 2]}
     >
-      <pointLight position={[10, 10, 5]} />
-      <pointLight position={[-10, -10, -5]} />
+      <pointLight position={[10, 10, 5]} intensity={1}  />
+      <pointLight position={[-10, -10, -5]} intensity={1}  />
       <ambientLight intensity={0.4} />
-      
-      <group position={[0, -1.5, 0]}>
-          <Model scale={2} position={[0, 2, 0]} castShadow receiveShadow />
 
-        <ContactShadows scale={10} blur={3} opacity={0.25} far={10} />
-      </group>
+
+<Model scale={1} position={[0, 0, 0]} castShadow receiveShadow />
 
       <Environment 
       preset="studio"
@@ -34,7 +31,8 @@ createRoot(document.getElementById('root')!).render(
       background resolution={64}>
 
       </Environment>
-              <OrbitControls />
+
+      <OrbitControls />
 
     </Canvas>
 
