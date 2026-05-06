94427358f82e57133fda97183b0dfa762f0fadca
Robin Andersson <danrobin.andersson@outlook.com>
2026-05-06 14:41:11 +0200

    added mock centralbank API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ApiTest } from "./dev/ApiTest";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Fantastic elastic panda</h1>
+      <ApiTest />
     </>
   );
 }
